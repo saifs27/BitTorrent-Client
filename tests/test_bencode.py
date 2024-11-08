1f25268d8f8c6e0250b7a3885467057c4de4d870
katzene <113651806+katzene@users.noreply.github.com>
2024-11-07 23:15:29 -0600

    update bencode tests

--- tests/test_bencode.py
+++ tests/test_bencode.py
@@ -7,9 +7,13 @@ def test_decode_int(data, expected):
     assert Decoder(data).decode_int() == expected
 
 
-@pytest.mark.parametrize("data,expected", [("2:hi", "hi"), ("4:test", "tes")])
+@pytest.mark.parametrize("data,expected", [("2:hi", "hi"), ("4:test", "test")])
 def test_decode_str(data, expected):
     assert Decoder(data).decode_string() == expected
+
+@pytest.mark.parametrize("data,expected", [("l2:hii17ee", ["hi", 17])])
+def test_decode_list(data, expected):
+    assert Decoder(data).decode_list() == expected
 
 
 
